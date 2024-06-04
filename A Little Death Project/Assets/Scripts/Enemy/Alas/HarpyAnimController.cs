48626409ef2240550f0327337a01500e8f18f907
MartinBetti <martin.betti@davinci.edu.ar>
2024-06-04 00:54:41 -0300

    Correciones UI

--- A Little Death Project/Assets/Scripts/Enemy/Alas/HarpyAnimController.cs
+++ A Little Death Project/Assets/Scripts/Enemy/Alas/HarpyAnimController.cs
@@ -1,7 +1,6 @@
 using DragonBones;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class HarpyAnimController : MonoBehaviour
