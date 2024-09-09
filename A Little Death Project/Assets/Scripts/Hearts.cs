e2df955fa650f4a55ece4840af98e0bfd4d4ab53
MartinBetti <martin.betti@davinci.edu.ar>
2024-09-09 20:37:24 -0300

    UI Flor

--- A Little Death Project/Assets/Scripts/Hearts.cs
+++ A Little Death Project/Assets/Scripts/Hearts.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -33,7 +34,8 @@ public class Hearts : MonoBehaviour
             }
             else
             {
-                hearts[i].sprite = emptyHeart;
+                //hearts[i].sprite = emptyHeart;
+                hearts[i].gameObject.SetActive(false);
             }
 
             if (i < numOfHearts)
