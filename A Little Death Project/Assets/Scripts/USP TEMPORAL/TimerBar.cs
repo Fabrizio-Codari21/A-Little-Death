86ae18f0692a8b2ea14962dea514823349b67fd5
TauPhika <berninimatias03@gmail.com>
2024-10-02 21:58:40 -0300

    Rehaciendo efecto de borde

--- A Little Death Project/Assets/Scripts/USP TEMPORAL/TimerBar.cs
+++ A Little Death Project/Assets/Scripts/USP TEMPORAL/TimerBar.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class TimerBar : MonoBehaviour
 {
     Image timer;
+    public Image border;
     [HideInInspector] public float maxTime = 10;
     [HideInInspector] public float timeLeft;
     public bool timerActive;
     public GameObject UI;
+    public float borderEffectSpeed;
+
+    Color _invisible;
+    Color _original;
 
     void Start()
     {
+        _invisible = new Color(1, 1, 1, 0);
+        _original = border.color;
+        
         timer = GetComponent<Image>();
         timeLeft = maxTime;
         UI.gameObject.SetActive(false);
@@ -23,6 +31,7 @@ public class TimerBar : MonoBehaviour
     //    timeLeft = maxTime;
     //}
 
+    bool _fadingIn;
     void Update()
     {
         if (timerActive == true)
@@ -31,6 +40,7 @@ public class TimerBar : MonoBehaviour
             {
                 timeLeft -= Time.deltaTime;
                 timer.fillAmount = timeLeft/maxTime;
+                BorderEffect(borderEffectSpeed * timeLeft);
             }
             else
             {
@@ -39,4 +49,17 @@ public class TimerBar : MonoBehaviour
             }
         }
     }
+
+    void BorderEffect(float speed)
+    {
+        if (_fadingIn) border.color = Color.Lerp(border.color, _original, ((timeLeft / speed) / maxTime));
+        else border.color = Color.Lerp(border.color, _invisible, ((timeLeft / speed) / maxTime));
+
+        print(border.color.a);
+
+        if (border.color.a <= 0.25f) _fadingIn = true;
+
+        else if (border.color.a >= 0.75f) _fadingIn = false;
+    }
+
 }
