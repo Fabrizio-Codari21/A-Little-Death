36977e1182cea6ad9d0ad7878b36037847877b6b
TauPhika <berninimatias03@gmail.com>
2024-11-07 16:45:39 -0300

    los checkpoint guardan automaticamente

--- A Little Death Project/Assets/Scripts/Menu.cs
+++ A Little Death Project/Assets/Scripts/Menu.cs
@@ -39,6 +39,7 @@ public class Menu : MonoBehaviour
                 if (SaveManager.allSaves[i] == default) { SaveManager.currentSave = i; break; }
             }
             StartCoroutine(waitForTransition("Level " + levelToLoad.ToString()));
+
         }
     }    
     
@@ -69,6 +70,21 @@ public class Menu : MonoBehaviour
         Checkpoints.active = false;
         Checkpoints.checkPoint = default;
         this.AsyncLoader(name);
+
+        var save = (SceneManager.GetActiveScene().buildIndex * 2) - 2;
+
+        var saveInfo = new SaveInfo(save,
+                                    Checkpoints.savedPos,
+                                    5);
+
+        if (saveInfo.sceneToKeep == string.Empty) { print("hubo un problema"); }
+
+        this.WaitAndThen(timeToWait: 1f, () =>
+        {
+            this.SaveGame(saveInfo, SaveManager.currentSave);
+        },
+        cancelCondition: () => false);
+        
     }
 
     IEnumerator waitForTransitionRestart()
