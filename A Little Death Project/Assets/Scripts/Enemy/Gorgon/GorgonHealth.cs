6781abba1ba985673c396a7e68de1e1d9bf9b628
MartinBetti <martin.betti@davinci.edu.ar>
2024-11-19 20:59:44 -0300

    Principio de setteo Gorgona

---
+++ A Little Death Project/Assets/Scripts/Enemy/Gorgon/GorgonHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GorgonHealth : PossessableHealth
+{
+    [SerializeField] int damage = 1;
+    [SerializeField] ParticleSystem[] particleSystems;
+    [SerializeField] FreeRoamMovement movementManager;
+
+    public override void Start()
+    {
+        base.Start();
+
+        movementManager = GetComponent<FreeRoamMovement>();
+    }
+
+    public override void Die()
+    {
+        foreach (var p in particleSystems)
+        {
+            Instantiate(p, transform.position, Quaternion.Euler(270, 180, 0));
+        }
+
+        base.Die();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player" && movementManager.canMove == true)
+        {
+            Debug.Log(movementManager.canMove);
+            var damageableObject = collision.gameObject.GetComponent<ThaniaHealth>();
+            if (!damageableObject.immune) damageableObject.Damage(gameObject, damage);
+        }
+    }
+}
---
+++ A Little Death Project/Assets/Scripts/Enemy/Gorgon/GorgonMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GorgonMovement : FreeRoamMovement
+{
+    [SerializeField] bool canSeePlayer = false;
+    [SerializeField] Vector2 LOS;
+    [SerializeField] Vector2 LOSOffset;
+    [SerializeField] public LayerMask playerLayer;
+
+    public float cooldown;
+    public ThaniaHealth player;
+
+    //public Animator animator;
+
+    private void Update()
+    {
+        touchingGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        touchingWall = Physics2D.OverlapCircle(wallCheck.position, 0.2f, groundLayer);
+
+        if (facingRight)
+        {
+            canSeePlayer = Physics2D.OverlapBox(transform.position + new Vector3(-LOSOffset.x, LOSOffset.y, 0), LOS, 0, playerLayer);
+        }
+        else if (!facingRight)
+        {
+            canSeePlayer = Physics2D.OverlapBox(transform.position + new Vector3(LOSOffset.x, LOSOffset.y, 0), LOS, 0, playerLayer);
+        }
+
+        cooldown -= Time.deltaTime;
+
+        if (canMove)
+        {
+            if (!canSeePlayer)
+            {
+                Patrol();
+            }
+            else if (canSeePlayer)
+            {
+                Rigidbody2D body = GetComponent<Rigidbody2D>();
+                body.velocity = Vector2.zero; 
+                //FlipToPlayer();
+                if (cooldown < 0)
+                {
+                    //Spit
+                }
+            }
+        }
+    }
+
+    public override void FixedUpdate()
+    {
+
+    }
+
+    void FlipToPlayer()
+    {
+        float distance = player.transform.position.x - transform.position.x;
+        if (distance < 0 && facingRight)
+        {
+            base.Flip();
+        }
+        else if (distance > 0 && !facingRight)
+        {
+            base.Flip();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (facingRight)
+        {
+            Gizmos.DrawWireCube(transform.position + new Vector3(-LOSOffset.x, LOSOffset.y, 0), LOS);
+        }
+        else if(!facingRight) 
+        {
+            Gizmos.DrawWireCube(transform.position + new Vector3(LOSOffset.x, LOSOffset.y, 0), LOS);
+
+        }
+    }
+}
