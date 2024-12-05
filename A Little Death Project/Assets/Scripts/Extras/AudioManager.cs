241b2226bfddb18928d80a82543ce21c79414d9f
TauPhika <berninimatias03@gmail.com>
2024-12-05 13:22:36 -0300

    tiene el audio manager nuevo, implementando sonidos

--- A Little Death Project/Assets/Scripts/Extras/AudioManager.cs
+++ A Little Death Project/Assets/Scripts/Extras/AudioManager.cs
@@ -9,5 +9,12 @@ public class AudioManager : MonoBehaviour
     public List<AudioSource> SFX = new();
 
     public AudioSource stunned;
+    public AudioSource possessionA;
+    public AudioSource possessionB;
+    public AudioSource groundDrop;
+    public AudioSource groundPound;
+    public AudioSource spit;
+    public AudioSource wallHit;
+
 
 }
